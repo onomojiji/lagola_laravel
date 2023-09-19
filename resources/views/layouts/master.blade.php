e6af791553a54f890ea7964c624d95e285bd312c
onomojiji <onomojj@gmail.com>
2023-09-19 16:38:28 +0100

    Intégration du topbar et du sidebar Okay

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!doctype html >
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="sm-hover" data-sidebar-image="none" data-preloader="disable">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="default" data-sidebar-image="none" data-preloader="enable">
 
 <head>
     <meta charset="utf-8" />
